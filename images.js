5f781a736ee4fe5cb3c87d609297f2d153b821d1
memirkocak <mekocak@edenschool.fr>
2024-06-03 17:14:37 +0200

    v2

---
+++ images.js
@@ -0,0 +1,103 @@
+export const db =[
+    {
+        "id": 1,
+        "category": "sport",
+        "url": "https://source.unsplash.com/200x300/?basketball"
+    },
+    {
+        "id": 2,
+        "category": "sport",
+        "url": "https://source.unsplash.com/200x300/?football"
+    },
+    {
+        "id": 3,
+        "category": "sport",
+        "url": "https://source.unsplash.com/200x300/?tennis"
+    },
+    {
+        "id": 4,
+        "category": "sport",
+        "url": "https://source.unsplash.com/200x300/?soccer"
+    },
+    {
+        "id": 5,
+        "category": "sport",
+        "url": "https://source.unsplash.com/200x300/?swimming"
+    },
+    {
+        "id": 6,
+        "category": "animal",
+        "url": "https://source.unsplash.com/200x300/?dog"
+    },
+    {
+        "id": 7,
+        "category": "animal",
+        "url": "https://source.unsplash.com/200x300/?cat"
+    },
+    {
+        "id": 8,
+        "category": "animal",
+        "url": "https://source.unsplash.com/200x300/?lion"
+    },
+    {
+        "id": 9,
+        "category": "animal",
+        "url": "https://source.unsplash.com/200x300/?tiger"
+    },
+    {
+        "id": 10,
+        "category": "animal",
+        "url": "https://source.unsplash.com/200x300/?elephant"
+    },
+    {
+        "id": 11,
+        "category": "nature",
+        "url": "https://source.unsplash.com/200x300/?forest"
+    },
+    {
+        "id": 12,
+        "category": "nature",
+        "url": "https://source.unsplash.com/200x300/?mountain"
+    },
+    {
+        "id": 13,
+        "category": "nature",
+        "url": "https://source.unsplash.com/200x300/?beach"
+    },
+    {
+        "id": 14,
+        "category": "nature",
+        "url": "https://source.unsplash.com/200x300/?river"
+    },
+    {
+        "id": 15,
+        "category": "nature",
+        "url": "https://source.unsplash.com/200x300/?waterfall"
+    },
+    {
+        "id": 16,
+        "category": "city",
+        "url": "https://source.unsplash.com/200x300/?new+york"
+    },
+    {
+        "id": 17,
+        "category": "city",
+        "url": "https://source.unsplash.com/200x300/?paris"
+    },
+    {
+        "id": 18,
+        "category": "city",
+        "url": "https://source.unsplash.com/200x300/?london"
+    },
+    {
+        "id": 19,
+        "category": "city",
+        "url": "https://source.unsplash.com/200x300/?tokyo"
+    },
+    {
+        "id": 20,
+        "category": "city",
+        "url": "https://source.unsplash.com/200x300/?dubai"
+    }
+  ]
+  
